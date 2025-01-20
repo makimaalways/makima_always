3ea2ae22cfe0bf2ea935c249fb20f658d75ec47f
makimaalways <rezeakuma76@gmail.com>
2025-01-20 17:25:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Makima Always - Karuta Activation & More",
+  title: "Makima Always - Karuta Activation & More Services",
   description: "Activate your Karuta account with ease on Discord. Get started with Makima Always, your ultimate helping hand!",
   icons: {
     icon: "pfp.png",
